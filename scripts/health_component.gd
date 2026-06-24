4623e6afb37ac7ef32c19d8f15e0f154ac9dd227
Zoltan Batoczki <zoltanbatoczki@gmail.com>
2026-06-23 17:52:32 -0700

    add health component

---
+++ scripts/health_component.gd
@@ -0,0 +1,25 @@
+class_name HealthComponent extends Node
+
+signal health_changed(currentHealth: int, maxHealth: int)
+signal died
+
+@export var max_health: int = 10
+@export var current_health: int = 0
+
+func _ready() -> void:
+	current_health = max_health
+	_emit()
+
+func _emit() -> void:
+	health_changed.emit(current_health, max_health)
+	print("HP: %d / %d" % [current_health, max_health])
+	
+func damage(amount: int) -> void:
+	current_health = clamp(current_health - amount, 0, max_health)
+	_emit()
+	if(current_health == 0):
+		died.emit()
+
+func heal(amount: int) -> void:
+	current_health = clamp(current_health + amount, 0, max_health)
+	_emit()
